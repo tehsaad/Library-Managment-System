43e6b6900dae2fbd17601fb28f2c6fe69b78d587
Saad Rizwan <tehsaad13453310@gmail.com>
2026-08-17 23:51:25 +0500

    .gitignore

--- Authorizer.cpp
+++ Authorizer.cpp
@@ -1,1 +1,39 @@
 #include "Authorizer.h"
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+
+int Authorizer::strLength(const char* text) {
+    if (!text) return 0;
+    int len = 0;
+    while (text[len] != '\0') ++len;      //Pre-Increment, dosen't inlcude Last NullPtr "\0"
+    return len;
+}
+
+bool Authorizer::isValid(int id) {
+    return id > 0;
+}
+
+bool Authorizer::isValidPassword(const char* password) {     //checks Null Pointers as well
+    return password && strLength(password) >= 6;
+}
+
+bool Authorizer::isValidContact(const char* contact) {
+    return contact && strLength(contact) >= 11;
+}
+
+bool Authorizer::isValidMenuChoice(int choice, int min, int max) {
+    return choice >= min && choice <= max;
+}
+
+int Authorizer::toInt(const char* text) {
+    if (!text) return 0;
+    return std::atoi(text);
+}
+
+
+
+
--- Authorizer.h
+++ Authorizer.h
@@ -1,5 +1,20 @@
 #pragma once
 class Authorizer
 {
+
+
+	bool isValid(int id);
+
+	bool isValidMenuChoice(int Choice, int min, int max);
+
+	bool isValidPassword(const char* password);
+
+	bool isValidContact(const char* contact);
+
+	int toInt(const char* text);
+
+	int strLength(const char* text);
+
+
 };
 
