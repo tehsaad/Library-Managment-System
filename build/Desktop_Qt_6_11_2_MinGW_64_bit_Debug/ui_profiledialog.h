cf8f8d1bce567cce4789b844fb5f55ddb2b91e63
Saad Rizwan <tehsaad13453310@gmail.com>
2026-09-06 13:01:15 +0500

    Qt, Updated
    Deleting the VS Community Branch, Making Classes in QT for .exe file

---
+++ build/Desktop_Qt_6_11_2_MinGW_64_bit_Debug/ui_profiledialog.h
@@ -0,0 +1,46 @@
+/********************************************************************************
+** Form generated from reading UI file 'profiledialog.ui'
+**
+** Created by: Qt User Interface Compiler version 6.11.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_PROFILEDIALOG_H
+#define UI_PROFILEDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_ProfileDialog
+{
+public:
+
+    void setupUi(QDialog *ProfileDialog)
+    {
+        if (ProfileDialog->objectName().isEmpty())
+            ProfileDialog->setObjectName("ProfileDialog");
+        ProfileDialog->resize(400, 300);
+
+        retranslateUi(ProfileDialog);
+
+        QMetaObject::connectSlotsByName(ProfileDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *ProfileDialog)
+    {
+        ProfileDialog->setWindowTitle(QCoreApplication::translate("ProfileDialog", "Dialog", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class ProfileDialog: public Ui_ProfileDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_PROFILEDIALOG_H
